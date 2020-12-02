72f708093d1868cfe1b8f499c82937ea5d7926a0
Darryl Pavan <darrylpavan@gmail.com>
2020-12-02 15:56:28 -0700

    Add Validation to signin page

--- parkingavailemployee/src/components/SignIn/SignIn.js
+++ parkingavailemployee/src/components/SignIn/SignIn.js
@@ -6,6 +6,8 @@ import Col from 'react-bootstrap/Col';
 import Row from 'react-bootstrap/Row';
 import { Redirect } from 'react-router-dom';
 import './SignIn.css';
+import { Formik } from 'formik'; 
+import * as yup from 'yup';
 
 class SignIn extends React.Component {
    constructor (props) {
@@ -31,7 +33,7 @@ class SignIn extends React.Component {
         this.setState({password: event.target.value});
     }
     
-    onSubmitSignIn = (event) => {
+    onSubmitSignIn = () => {
         // Call/signin passing email address and password in the request body, POST, returns a user object.
         const postOptions = {
             method: 'POST',
@@ -96,6 +98,15 @@ class SignIn extends React.Component {
         if (this.state.redirect) {
             return  <Redirect to={{ pathname: this.state.redirect }}/>
         }
+        //email and password validation rules using "yup"
+        const schema = yup.object({
+            email: yup.string()
+                        .email("Invalid email address format")
+                        .required("Email is required"),
+            password: yup.string()
+                        .min(4, "Password must be 4 characters at minimum")
+                        .required("Password is required")          
+            });
 
         return (
         
@@ -109,20 +120,49 @@ class SignIn extends React.Component {
                     </Row>
                     <Row>
                     <Col xs ={7} md={4} lg={4} className = "mt-2 mb-3">                  
-                                                        
-                        <Form>
-                            <Form.Group controlId="formBasicEmail">
+                        <Formik
+                            validationSchema={schema}
+                            onSubmit={values =>
+                                {
+                                    this.setState({email:values.email, password: values.password});
+                                    this.onSubmitSignIn();
+                                }} 
+                            initialValues={{
+                            email: "",
+                            password: ""
+                            }}
+                        >
+                            {({
+                                handleSubmit,
+                                handleChange,
+                                handleBlur,
+                                values,
+                                touched,
+                                isValid,
+                                errors,
+                            }) => (                               
+                                <Form noValidate onSubmit = {handleSubmit}>
+                                    <Form.Group controlId="formBasicEmail">
 
-                            <Form.Label>Email address</Form.Label>
-                                <Form.Control type="email" placeholder="Enter email" onChange = {this.onEmailChange}/>
-                            </Form.Group>
+                                    <Form.Label>Email address</Form.Label>
+                                        <Form.Control type="email" name = "email" placeholder="Enter email" onChange = {handleChange} value = {values.email} isInvalid={!!errors.email}/>
+                                        <Form.Control.Feedback type="invalid">
+                                        {errors.email}
+                                        </Form.Control.Feedback>
+                                    </Form.Group>
 
-                            <Form.Group controlId="formBasicPassword">
-                                <Form.Label>Password</Form.Label>
-                                <Form.Control type="password" placeholder="Password" onChange = {this.onPasswordChange}/>
-                            </Form.Group>
-                            <Button variant="primary" onClick = {this.onSubmitSignIn}>Sign In</Button>
-                        </Form>
+
+                                    <Form.Group controlId="formBasicPassword">
+                                        <Form.Label>Password</Form.Label>
+                                        <Form.Control type="password" name = "password" placeholder="Password" onChange = {handleChange} value = {values.password} isInvalid={!!errors.password}/>
+                                        <Form.Control.Feedback type="invalid">
+                                        {errors.password}
+                                        </Form.Control.Feedback>
+                                    </Form.Group>
+                                    <Button type = "submit" variant="primary">Sign In</Button>
+                                </Form>
+                            )}
+                        </Formik>
                     </Col>
                     </Row>
                 </Col>
